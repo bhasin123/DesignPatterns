1c6d16c8374fd5d7f56c2504bd95695bb93494c4
bhasin123 <bhasin.rohit@yahoo.com>
2024-12-03 06:20:13 +0530

    Singleton Pattern

---
+++ SingletonPattern/Counter.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonPattern
+{
+    internal class Counter
+    {
+        public static int Count { get; private set; }
+
+        private static Counter _instance;
+        private static object lockObj = new object();
+
+        private Counter()
+        {
+        }
+
+        public static Counter Initialize()
+        {
+            if(_instance == null)
+            {
+                lock(lockObj)
+                {
+                    if (_instance == null)
+                    {
+                        _instance = new Counter();
+                        Count = 0;
+                    }
+                }
+            }
+
+            return _instance;
+        }
+
+        public Counter GetInstance()
+        {
+            return _instance;
+        }
+
+
+        public void Increment()
+        {
+            Count++;
+            Console.WriteLine($"Count is now {Count}, ThreadId - {Thread.CurrentThread.ManagedThreadId}");
+        }
+
+    }
+}
---
+++ SingletonPattern/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace SingletonPattern
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter counter;
+
+            Parallel.For(0, 100, i =>
+            {
+                counter = Counter.Initialize();
+                Parallel.For(0, 10, j =>
+                {
+                    counter.Increment();
+                });
+            });
+        }
+
+    }
+}
