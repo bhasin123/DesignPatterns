2dbb8e8a1640b92896ef08b39fd165e236b8c2bd
Rohit Bhasin <rohit.bhasin@businessnext.com>
2025-08-21 07:05:51 +0530

    ProtoType Pattern

---
+++ ProtoTypePattern/Employee.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProtoTypePattern
+{
+    internal class Employee : IPrototype<Employee>
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Designation { get; set; }
+        public Address CurrentAddress { get; set; }
+        public Address PermanentAddress { get; set; }
+
+        public Employee Clone()
+        {
+            return (Employee)this.MemberwiseClone();
+        }
+
+        public override string ToString()
+        {
+            return $"Name - {Name}, Age - {Age}, Designation - {Designation}, CurrentAddress - ({CurrentAddress.Street},{CurrentAddress.City},{CurrentAddress.State}, {CurrentAddress.ZipCode}), PermanentAddress - ({PermanentAddress.Street},{PermanentAddress.City},{PermanentAddress.State}, {PermanentAddress.ZipCode}) ";
+        }
+    }
+
+    internal class Address
+    {
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+    }
+}
---
+++ ProtoTypePattern/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace ProtoTypePattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, We are implementing Prototype Pattern!");
+
+            var emp1 = new Employee
+            {
+                Name = "Rohit Bhasin",
+                Age = 30,
+                Designation = "Software Engineer",
+                CurrentAddress = new Address
+                {
+                    Street = "123 Main St",
+                    City = "Springfield",
+                    State = "IL",
+                    ZipCode = "62701"
+                },
+                PermanentAddress = new Address
+                {
+                    Street = "456 Elm St",
+                    City = "Springfield",
+                    State = "IL",
+                    ZipCode = "62702"
+                }
+            };
+
+            Console.WriteLine($"Original Employee: {emp1.ToString()}");
+
+            // Cloning the employee
+            var emp2 = emp1.Clone();
+
+            Console.WriteLine(emp2.ToString());
+
+            Console.WriteLine("Modifying the original employee's Address and cloning Name...");
+
+            emp1.CurrentAddress.Street = "Crossing";
+            emp1.CurrentAddress.City = "Noida";
+            emp1.CurrentAddress.State = "UP";
+            emp1.CurrentAddress.ZipCode = "201016";
+
+            emp2.Name = "Reyansh Bhasin";
+
+            emp2.PermanentAddress = emp1.CurrentAddress;
+
+            Console.WriteLine($"Original Employee: {emp1.ToString()}");
+
+            Console.WriteLine($"Cloned Employee: {emp2.ToString()}");
+
+            Console.ReadKey();
+
+        }
+    }
+}
